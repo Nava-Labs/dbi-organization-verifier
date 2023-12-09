6055f7e082112dc04e8dd49cda9346ddabb6e551
rwin <erwinphanglius10@gmail.com>
2023-12-09 22:53:52 +0700

    fix: auth reason

--- proofRequest.js
+++ proofRequest.js
@@ -2,19 +2,10 @@ const { DBIOrganizationCredential } = require("./vcHelpers/DBIOrganizationCreden
 
 // design your own customised authentication requirement here using Query Language
 // https://0xpolygonid.github.io/tutorials/verifier/verification-library/zk-query-language/
-
-const humanReadableAuthReason = "Must be born before this year";
+const humanReadableAuthReason = "Must be in Orgz";
 
 const credentialSubject = {
-  // birthday: {
-  //   // users must be born before this year
-  //   // birthday is less than Jan 1, 2023
-  //   $lt: 20230101,
-  // },
-
   organizationId: {
-    // users must be born before this year
-    // birthday is less than Jan 1, 2023
     $eq: 1,
   },
 
